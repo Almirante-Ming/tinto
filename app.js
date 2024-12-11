f1303293644dbdaeb9636bdc796eb19dd54d8825
v. guilherme <v.guilherme.barreto@gmail.com>
2024-12-11 13:14:18 -0400

    added tailwind.config

--- app.js
+++ app.js
@@ -5,9 +5,21 @@ const port = 3101;
 tinto.get('/', (req,res) => {
     res.sendFile(__dirname + '/views/index.html')
 });
+tinto.get('/teste', (req,res) => {
+    res.send('it`s works')
+});
 
 tinto.get('/notes/new', (req,res) => {
     res.sendFile(__dirname + '/views/new_note.html')
 });
+
+tinto.get('/notes/closed', (req, res) =>{
+    res.sendFile(__dirname+'/view/append.html')
+});
+
+tinto.get('/tasks/new', (req, res) => {
+    res.sendFile(__dirname+'/views/new_task.html')
+});
+
 
 tinto.listen(port, () => console.log(`Server running on port ${port}`));
